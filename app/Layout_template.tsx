e09bd2b84a0073632b6f1f39f54df0712dbe1d01
hristiyan08 <hristianjordanov08@gmail.com>
2025-07-01 22:08:02 +0300

    fixing Cloudflare's errors

--- app/Layout_template.tsx
+++ app/Layout_template.tsx
@@ -1,46 +1,66 @@
 'use client'
 
-import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
-import { ArchiveBoxIcon , DocumentTextIcon } from '@heroicons/react/24/outline'
-import {getCurrentProfileInformation} from '../app/data/DataInfo';
+import { useEffect, useState } from 'react'
+import {
+    Disclosure,
+    DisclosureButton,
+    DisclosurePanel,
+    Menu,
+    MenuButton,
+    MenuItem,
+    MenuItems
+} from '@headlessui/react'
+import {
+    Bars3Icon,
+    BellIcon,
+    XMarkIcon,
+    ArchiveBoxIcon,
+    DocumentTextIcon
+} from '@heroicons/react/24/outline'
+import { getCurrentProfileInformation } from '../app/data/DataInfo'
+
 const user = {
     name: 'Tom Cook',
     email: 'tom@example.com',
-    imageUrl:
-        'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    imageUrl: 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
 }
 
 const navigation = [
-    { name: 'Начало', href: '..',  },
-    { name: 'Продажби', href: 'orders',  },
-    { name: 'Продукти', href: 'products',  },
-    { name: 'Акаунти', href: 'profiles',  },
-
+    { name: 'Начало', href: '..' },
+    { name: 'Продажби', href: 'orders' },
+    { name: 'Продукти', href: 'products' },
+    { name: 'Акаунти', href: 'profiles' },
 ]
 
 const userNavigation = [
     { name: 'Your Profile', href: '#' },
     { name: 'Settings', href: '#' },
     { name: 'Sign out', href: '../signout' },
-
 ]
+
 function classNames(...classes: (string | boolean | undefined | null)[]): string {
-    return classes.filter(Boolean).join(' ');
+    return classes.filter(Boolean).join(' ')
 }
 
-
-const userData = await getCurrentProfileInformation();
 export default function Layout_template({ children, title }: LayoutProps) {
+    const [location, setLocation] = useState('')
+
+    useEffect(() => {
+        async function fetchData() {
+            const data = await getCurrentProfileInformation()
+            setLocation(data.location || '')
+        }
+        fetchData()
+    }, [])
+
     return (
         <div className="min-h-full">
             <Disclosure as="nav" className="bg-gray-900">
-
                 <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                     <div className="flex h-20 items-center justify-between pt-2">
                         <div className="flex items-center">
                             <div className="shrink-0">
-                                <h1 className="text-white text-3xl font-bold cursor-pointer" onClick={() => window.location.href='..'}>San Marco VT</h1>
+                                <h1 className="text-white text-3xl font-bold cursor-pointer" onClick={() => window.location.href = '..'}>San Marco VT</h1>
                             </div>
                             <div className="hidden md:block">
                                 <div className="ml-60 flex items-baseline space-x-4">
@@ -48,10 +68,9 @@ export default function Layout_template({ children, title }: LayoutProps) {
                                         <a
                                             key={item.name}
                                             href={item.href}
-                                            aria-current={item.current ? 'page' : undefined}
                                             className={classNames(
-                                                item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-800 hover:text-white',
-                                                'rounded-md px-3 py-2 text-md font-medium',
+                                                'text-gray-300 hover:bg-gray-800 hover:text-white',
+                                                'rounded-md px-3 py-2 text-md font-medium'
                                             )}
                                         >
                                             {item.name}
@@ -64,31 +83,24 @@ export default function Layout_template({ children, title }: LayoutProps) {
                             <div className="ml-4 flex items-center md:ml-6">
                                 <button
                                     type="button"
-                                    onClick={()=> window.location.href='/history'}
-                                    className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden"
+                                    onClick={() => window.location.href = '/history'}
+                                    className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                                 >
-                                    <span className="absolute -inset-1.5" />
-                                    <span className="sr-only">View notifications</span>
-                                    <BellIcon aria-hidden="true" className="size-6" />
+                                    <BellIcon className="size-6" aria-hidden="true" />
                                 </button>
 
                                 <Menu as="div" className="relative ml-3">
                                     <div>
-                                        <MenuButton className="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden">
-                                            <span className="absolute -inset-1.5" />
-                                            <span className="sr-only">Open user menu</span>
-                                            <img alt="" src={user.imageUrl} className="size-8 rounded-full" />
+                                        <MenuButton className="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                                            <img className="size-8 rounded-full" src={user.imageUrl} alt="" />
                                         </MenuButton>
                                     </div>
-                                    <MenuItems
-                                        transition
-                                        className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 ring-1 shadow-lg ring-black/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
-                                    >
+                                    <MenuItems className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 ring-1 ring-black/5 shadow-lg">
                                         {userNavigation.map((item) => (
                                             <MenuItem key={item.name}>
                                                 <a
                                                     href={item.href}
-                                                    className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
+                                                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                                                 >
                                                     {item.name}
                                                 </a>
@@ -99,11 +111,9 @@ export default function Layout_template({ children, title }: LayoutProps) {
                             </div>
                         </div>
                         <div className="-mr-2 flex md:hidden">
-                            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden">
-                                <span className="absolute -inset-0.5" />
-                                <span className="sr-only">Open main menu</span>
-                                <Bars3Icon aria-hidden="true" className="block size-6 group-data-open:hidden" />
-                                <XMarkIcon aria-hidden="true" className="hidden size-6 group-data-open:block" />
+                            <DisclosureButton className="inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white">
+                                <Bars3Icon className="block size-6" aria-hidden="true" />
+                                <XMarkIcon className="hidden size-6" aria-hidden="true" />
                             </DisclosureButton>
                         </div>
                     </div>
@@ -116,11 +126,7 @@ export default function Layout_template({ children, title }: LayoutProps) {
                                 key={item.name}
                                 as="a"
                                 href={item.href}
-                                aria-current={item.current ? 'page' : undefined}
-                                className={classNames(
-                                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                                    'block rounded-md px-3 py-2 text-base font-medium',
-                                )}
+                                className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
                             >
                                 {item.name}
                             </DisclosureButton>
@@ -128,21 +134,17 @@ export default function Layout_template({ children, title }: LayoutProps) {
                     </div>
                     <div className="border-t border-gray-700 pt-4 pb-3">
                         <div className="flex items-center px-5">
-                            <div className="shrink-0">
-                                <img alt="" src={user.imageUrl} className="size-10 rounded-full" />
-                            </div>
+                            <img className="size-10 rounded-full" src={user.imageUrl} alt="" />
                             <div className="ml-3">
-                                <div className="text-base/5 font-medium text-white">{user.name}</div>
+                                <div className="text-base font-medium text-white">{user.name}</div>
                                 <div className="text-sm font-medium text-gray-400">{user.email}</div>
                             </div>
                             <button
                                 type="button"
-                                onClick={()=> window.location.href='/history'}
-                                className="relative ml-auto shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden"
+                                onClick={() => window.location.href = '/history'}
+                                className="ml-auto shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white"
                             >
-                                <span className="absolute -inset-1.5" />
-                                <span className="sr-only">View notifications</span>
-                                <BellIcon aria-hidden="true" className="size-6" />
+                                <BellIcon className="size-6" aria-hidden="true" />
                             </button>
                         </div>
                         <div className="mt-3 space-y-1 px-2">
@@ -164,14 +166,16 @@ export default function Layout_template({ children, title }: LayoutProps) {
             <header className="bg-white shadow-sm">
                 <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 flex row justify-between">
                     <h1 className="text-2xl font-bold tracking-tight text-gray-900">{title}</h1>
-                    <h1 className="text-2xl  tracking-tight text-gray-900"> {userData.location}</h1>
+                    <h1 className="text-2xl tracking-tight text-gray-900">{location}</h1>
                 </div>
             </header>
+
             <main>
                 <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
                     {children}
                 </div>
             </main>
+
             {/* Newsletter Section */}
             <div className="relative isolate overflow-hidden bg-gray-900 py-16 sm:py-24 lg:py-32">
                 <div className="mx-auto max-w-7xl px-6 lg:px-8">
@@ -181,44 +185,40 @@ export default function Layout_template({ children, title }: LayoutProps) {
                             <p className="mt-4 text-lg text-gray-300">
                                 Админ система за направа на стокови разписки и фактури, управление на складова програма.
                             </p>
-
                         </div>
                         <dl className="grid grid-cols-1 gap-x-8 gap-y-10 sm:grid-cols-2 lg:pt-2">
                             <div className="flex flex-col items-start">
                                 <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
-                                    <DocumentTextIcon  aria-hidden="true" className="size-6 text-white" />
+                                    <DocumentTextIcon className="size-6 text-white" aria-hidden="true" />
                                 </div>
                                 <dt className="mt-4 text-base font-semibold text-white">Стокова разпизка</dt>
-                                <dd className="mt-2 text-base/7 text-gray-400">
+                                <dd className="mt-2 text-base text-gray-400">
                                     За да направите стокова разписка отивате на дадения раздел и избирате "Добавяне на нова".
                                 </dd>
                             </div>
                             <div className="flex flex-col items-start">
                                 <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
-                                    <ArchiveBoxIcon  aria-hidden="true" className="size-6 text-white" />
+                                    <ArchiveBoxIcon className="size-6 text-white" aria-hidden="true" />
                                 </div>
                                 <dt className="mt-4 text-base font-semibold text-white">Складова програма</dt>
-                                <dd className="mt-2 text-base/7 text-gray-400">
-                                    Можете да добавяте нови продукти и да ги редактирате от дадения раздел. Цените също се редактират от там
+                                <dd className="mt-2 text-base text-gray-400">
+                                    Можете да добавяте нови продукти и да ги редактирате от дадения раздел. Цените също се редактират от там.
                                 </dd>
                             </div>
                         </dl>
                     </div>
-
                 </div>
                 <div aria-hidden="true" className="absolute top-0 left-1/2 -z-10 -translate-x-1/2 blur-3xl xl:-top-6">
                     <div
+                        className="aspect-1155/678 w-[72.1875rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
                         style={{
                             clipPath:
                                 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
                         }}
-                        className="aspect-1155/678 w-[72.1875rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
                     />
                 </div>
-                <h1 className="text-white text-center relative top-15">Made by Hristiyan Yordanov</h1>
+                <h1 className="text-white text-center mt-10">Made by Hristiyan Yordanov</h1>
             </div>
-
         </div>
-
     )
 }
