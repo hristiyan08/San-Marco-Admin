c126cdc6cacbb581e5a86dd8981be10b92647976
hristiyan08 <hristianjordanov08@gmail.com>
2025-07-02 16:43:20 +0300

    Update page.tsx

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -3,7 +3,7 @@ import Layout_template from "@/app/Layout_template";
 export default function Orders() {
     return (
         <Layout_template title="Поръчки">
-            
+            <h1>Hello</h1>
         </Layout_template>
     )
 }
