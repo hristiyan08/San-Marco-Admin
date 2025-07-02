5e2f02b06d3a441b3f089fd4d2abac7c96661bd3
hristiyan08 <hristianjordanov08@gmail.com>
2025-07-02 18:35:52 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    // други опции, ако имаш, но без output: 'export'
+    reactStrictMode: true,
+    // ... други настройки по желание
 };
 
 export default nextConfig;
