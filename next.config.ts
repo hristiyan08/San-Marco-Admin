6f384e0a6d1014da6964771e06984f28415c5a59
hristiyan08 <hristianjordanov08@gmail.com>
2025-07-02 17:38:09 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig = {
-    output: 'standalone',
-    experimental: {
-        instrumentationHook: false
-    }
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
